287e8258a934187b12bff72eb0d4b7f777955c74
Chanpreet Singh <ch835893@dal.ca>
2022-08-10 18:46:36 -0300

    Neptune Creds

--- Project/constants.py
+++ Project/constants.py
@@ -1,6 +1,6 @@
 # Neptune
 project_id = "csci-5901-final-project/final-project"
-api_token = "eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiJlMGM2Y2M2ZC1iZDcyLTQxMjUtODgyOS02NDM0MzFmZjY0Y2EifQ==",
+api_token = "eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiJlMGM2Y2M2ZC1iZDcyLTQxMjUtODgyOS02NDM0MzFmZjY0Y2EifQ=="
 
 # Model
 model_output_file = "model.h5"
